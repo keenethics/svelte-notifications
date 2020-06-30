66f6aacad99c7d18ce125af36a91d6223567cdc1
axtn95 <axtn95@gmail.com>
2020-06-30 10:35:27 -0700

    Expose notificationsStore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Notifications from './components/Notifications.svelte';
+import { getNotificationsContext } from './context';
+import notificationsStore from './store';
 
-export { getNotificationsContext } from './context';
+export { getNotificationsContext, notificationsStore } from './context';
 
 export default Notifications;
